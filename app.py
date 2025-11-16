18809494ea76c5ac8c8ef4a6a44579387bc0d919
fridazccy <cklauba@connect.ust.hk>
2025-11-16 16:55:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,1 @@
-import os
-import numpy as np
-import pandas as pd
-from openai import AzureOpenAI
 
-openai_client = AzureOpenAI(
-  api_key = "AZURE_API_KEY", # use your key here
-  api_version = "2023-05-15", # apparently HKUST uses a deprecated version
-  azure_endpoint = "https://hkust.azure-api.net" # per HKUST instructions
-)
